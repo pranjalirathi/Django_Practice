cfadbad966eae03fc07b821788fdac0c25b0ee24
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-06-20 16:27:27 +0530

    rg_8

--- calc/views.py
+++ calc/views.py
@@ -10,7 +10,13 @@ def home(request):
     return render(request, 'home.html', {'name': 'prashu'})
 
 def add(request):
-    val1  = int(request.GET['num1'])
-    val2  = int(request.GET['num2'])
+    val1  = int(request.POST['num1'])
+    val2  = int(request.POST['num2'])
     res = val1 + val2
     return render(request, 'result.html', {'result' : res})
+
+
+#HTTP METHODS
+#GET: whenever we want to fetch something from the server and we also have to pass some data on the server
+#to not show the query parameters on the address bar, we have to use the post method'''
+#POST: Whenever we want to send some data on the server
