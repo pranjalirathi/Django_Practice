a19766a86bc0f64cd13168e73f939f55011fdcb7
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-06-21 12:26:19 +0530

    14_dynamic_data

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# defining a class structure here and the object will be instanced in the views.py
+class Destination: 
+    id: int
+    name: str
+    img : str
+    desc: str
+    price: int
+
--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    
+    #creating the object 
+    dest1 = Destination();
+    dest2 = Destination();
+    dest3 = Destination();
+
+    #passing the values
+    dest1.name = "Rawatbhata"
+    dest1.desc = "The power plant city"
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2.name = "Kota"
+    dest2.desc = "The study city"
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 800
+
+    dest3.name = "Chittor"
+    dest3.desc = "The fort city"
+    dest3.img= 'destination_3.jpg'
+    dest3.price = 900
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, "index.html", {'dests': dests})
